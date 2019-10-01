98536bf483f5cebadc0fd4af5bffa76348f90d47
Zer0Momentum <zer0momentum@protonmail.com>
2019-09-30 21:15:50 -0300

    added new files

---
+++ train/init.py
@@ -0,0 +1,166 @@
+"""
+    Works on tensorflow version 1.14.0 
+    for version 2.0 and beyond: change the keys for acc to accuracy
+    in model.history[]
+
+"""
+
+import tensorflow as tf
+import os
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+
+base_dir = os.path.join('data')
+print(base_dir)
+IMAGE_SIZE = 224
+BATCH_SIZE = 64
+
+datagen = tf.keras.preprocessing.image.ImageDataGenerator(
+    rescale=1. / 255,
+    validation_split=0.2)
+
+train_generator = datagen.flow_from_directory(
+    base_dir,
+    target_size=(IMAGE_SIZE, IMAGE_SIZE),
+    batch_size=BATCH_SIZE,
+    subset='training')
+
+val_generator = datagen.flow_from_directory(
+    base_dir,
+    target_size=(IMAGE_SIZE, IMAGE_SIZE),
+    batch_size=BATCH_SIZE,
+    subset='validation')
+print(val_generator)
+for image_batch, label_batch in train_generator:
+    break
+image_batch.shape, label_batch.shape
+
+print(train_generator.class_indices)
+
+labels = '\n'.join(sorted(train_generator.class_indices.keys()))
+
+with open('labels.txt', 'w') as f:
+    f.write(labels)
+
+IMG_SHAPE = (IMAGE_SIZE, IMAGE_SIZE, 3)
+
+# Create the base model from the pre-trained model MobileNet V2
+base_model = tf.keras.applications.MobileNetV2(input_shape=IMG_SHAPE,
+                                               include_top=False,
+                                               weights='imagenet')
+
+base_model.trainable = False
+
+model = tf.keras.Sequential([
+    base_model,
+    tf.keras.layers.Conv2D(32, 3, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.GlobalAveragePooling2D(),
+    tf.keras.layers.Dense(5, activation='softmax')
+])
+
+model.compile(optimizer=tf.keras.optimizers.Adam(),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.summary()
+
+print('Number of trainable variables = {}'.format(len(model.trainable_variables)))
+
+epochs = 12
+
+model_hist = model.fit_generator(train_generator,
+                                 steps_per_epoch=10,
+                                 epochs=epochs,
+                                 validation_data=val_generator)
+print(model_hist)
+# filename = "saved_model.pkl"
+
+# pickle.dump(model, open(filename, 'wb'))
+
+# model = pickle.load(open(filename, 'rb'))
+
+acc = model_hist.history['acc']
+val_acc = model_hist.history['val_acc']
+
+loss = model_hist.history['loss']
+val_loss = model_hist.history['val_loss']
+
+plt.figure(figsize=(8, 8))
+plt.subplot(2, 1, 1)
+plt.plot(acc, label='Training Accuracy')
+plt.plot(val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.ylabel('Accuracy')
+plt.ylim([min(plt.ylim()), 1])
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(2, 1, 2)
+plt.plot(loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.ylabel('Cross Entropy')
+plt.ylim([0, 1.0])
+plt.title('Training and Validation Loss')
+plt.xlabel('epoch')
+plt.show()
+
+base_model.trainable = True
+
+# Let's take a look to see how many layers are in the base model
+print("Number of layers in the base model: ", len(base_model.layers))
+
+# Fine tune from this layer onwards
+fine_tune_at = 100
+
+# Freeze all the layers before the `fine_tune_at` layer
+for layer in base_model.layers[:fine_tune_at]:
+    layer.trainable = False
+
+model.compile(loss='sparse_categorical_crossentropy',
+              optimizer=tf.keras.optimizers.Adam(1e-5),
+              metrics=['accuracy'])
+
+model.summary()
+
+print('Number of trainable variables = {}'.format(len(model.trainable_variables)))
+
+history_fine = model.fit_generator(train_generator,
+                                   epochs=5,
+                                   validation_data=val_generator)
+
+saved_model_dir = 'save/fine_tuning'
+tf.saved_model.save(model, saved_model_dir)
+
+
+converter = tf.lite.TFLiteConverter.from_saved_model(saved_model_dir)
+tflite_model = converter.convert()
+
+with open('model.tflite', 'wb') as f:
+    f.write(tflite_model)
+
+acc = history_fine.history['acc']
+val_acc = history_fine.history['val_acc']
+
+loss = history_fine.history['loss']
+val_loss = history_fine.history['val_loss']
+
+plt.figure(figsize=(8, 8))
+plt.subplot(2, 1, 1)
+plt.plot(acc, label='Training Accuracy')
+plt.plot(val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.ylabel('Accuracy')
+plt.ylim([min(plt.ylim()), 1])
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(2, 1, 2)
+plt.plot(loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.ylabel('Cross Entropy')
+plt.ylim([0, 1.0])
+plt.title('Training and Validation Loss')
+plt.xlabel('epoch')
+plt.show()
